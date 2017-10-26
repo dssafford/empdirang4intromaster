247449c87065c3dffb156cd31b25048d89431624
Doug Safford <doug.safford@yahoo.com>
2017-10-26 07:34:20 -0500

    works, all tests work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+		<h1>{{title}}</h1>
+		<router-outlet></router-outlet>
+	`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  name = 'Employee Directory';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppComponent } from './app.component';
 import {EmployeeAddComponent} from './employees/add/employee-add.component';
 import {EmployeeEditComponent} from './employees/edit/employee-edit.component';
 import {EmployeesComponent} from './employees/employees.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {EmployeeService} from './employees/services/employee.service';
+import {RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent, EmployeeAddComponent, EmployeeEditComponent, EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, RouterModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
